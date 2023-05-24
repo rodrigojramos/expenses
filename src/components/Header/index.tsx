36de627e3d4bb74411402da0fd7f28bcce3c36de
Rodrigo Ramos <digojramos@gmail.com>
2023-05-24 11:39:23 -0300

    Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css';
+import LogoIcon from '../../assets/logo.png';
+
+export default function Header() {
+    return (
+        <div className="exp-header-container">
+            <div className="exp-container">
+                <div className="exp-header-content">
+                    <img className="exp-header-logo" src={LogoIcon} alt="Logo" />
+                    <div className="exp-header-title">
+                        <h1>Controle de gastos</h1>
+                        <p>Desenvolvido por Rodrigo Ramos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
