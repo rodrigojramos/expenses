3480e953a86e0946951f3e3f81aa2440262a191e
Rodrigo Ramos <digojramos@gmail.com>
2023-05-26 10:48:42 -0300

    Table expenses created

--- src/routes/Home/Expenses/index.tsx
+++ src/routes/Home/Expenses/index.tsx
@@ -1,7 +1,10 @@
 import SelectDate from '../../../components/SelectDate';
 import './styles.css';
+import DeleteIcon from '../../../assets/delete.svg';
+import EditIcon from '../../../assets/edit.svg';
 
 export default function Expenses() {
+
     return(
         <main>
             <section className="exp-expenses-section">
@@ -10,6 +13,40 @@ export default function Expenses() {
                         <h2>Despesas</h2>
                         <div>
                             <SelectDate />
+                        </div>
+                        <div>
+                            <table className="exp-table">
+                                <thead>
+                                    <th className="exp-padding">Dia</th>
+                                    <th className="exp-txt-left scg-padding">Descrição</th>
+                                    <th className="exp-padding">Valor</th>
+                                    <th></th>
+                                    <th></th>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                        <td className="exp-padding">26</td>
+                                        <td className="exp-txt-left scg-padding">Conta de água</td>
+                                        <td className="exp-padding">R$ 200,00</td>
+                                        <td className="exp-padding"><img src={EditIcon} alt="Editar" /></td>
+                                        <td><img src={DeleteIcon} alt="Deletar" /></td>
+                                    </tr>
+                                    <tr>
+                                        <td className="exp-padding">26</td>
+                                        <td className="exp-txt-left scg-padding">Conta de água</td>
+                                        <td className="exp-padding">R$ 200,00</td>
+                                        <td className="exp-padding"><img src={EditIcon} alt="Editar" /></td>
+                                        <td><img src={DeleteIcon} alt="Deletar" /></td>
+                                    </tr>
+                                    <tr>
+                                        <td className="exp-padding">26</td>
+                                        <td className="exp-txt-left scg-padding">Conta de água</td>
+                                        <td className="exp-padding">R$ 200,00</td>
+                                        <td className="exp-padding"><img src={EditIcon} alt="Editar" /></td>
+                                        <td><img src={DeleteIcon} alt="Deletar" /></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
